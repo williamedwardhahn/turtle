c397661a172c8213309c979af0f98443ea34ca83
William E Hahn <williamedwardhahn@gmail.com>
2020-12-01 11:36:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name='turtle',
-      version='0.1',
-      description='Turtle Net',
-      url='https://github.com/williamedwardhahn/turtle',
-      author='W Hahn',
-      author_email='williamedwardhahn@gmail.com',
-      license='MIT',
-      packages=['turtle'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+setuptools.setup(
+    name="turtle",
+    version="0.0.1",
+    author="William Edward Hahn",
+    author_email="wiliamedwardhahn@gmail.com",
+    description="Functional Pytorch: Turtles all the way down",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wiliamedwardhahn/turtle",
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
