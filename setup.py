f6529c0def35b0334f7b86a45e737f6ecad4921b
William E Hahn <williamedwardhahn@gmail.com>
2020-12-01 11:16:31 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='turtle',
+      version='0.1',
+      description='Turtle Net',
+      url='https://github.com/williamedwardhahn/turtle',
+      author='W Hahn',
+      author_email='williamedwardhahn@gmail.com',
+      license='MIT',
+      packages=['turtle'],
+      zip_safe=False)
